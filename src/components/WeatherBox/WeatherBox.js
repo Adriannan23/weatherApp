f80284ba44749416016164390c3854095f5ec6b6
Adrianna Nowak <adriannanowakk@gmail.com>
2023-09-08 14:36:20 +0200

    add pending and error states

--- src/components/WeatherBox/WeatherBox.js
+++ src/components/WeatherBox/WeatherBox.js
@@ -2,32 +2,48 @@ import PickCity from '../PickCity/PickCity';
 import WeatherSummary from '../WeatherSummary/WeatherSummary';
 import Loader from '../Loader/Loader';
 import { useCallback, useState } from 'react';
+import ErrorBox from '../ErrorBox/ErrorBox';
 
 const WeatherBox = props => {
 
-  const [weatherData, setWeatherData] = useState({});
+  const [weatherData, setWeatherData] = useState('');
+
+  const [pending, setPending] = useState(false);
+
+  const [error, setError] = useState(false);
 
   const handleCityChange = useCallback((city) => {
-    console.log(city)
-
+    setPending(true);
+    setError(false);
     fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=8b57e8c62ff6803ac4e8696037a21103&units=metric`)
-      .then(res => res.json())
-      .then(data => {
-        console.log(data);
-        const weatherData2 = {
-          city: data.name,
-          temp: data.main.temp,
-          icon: data.weather[0].icon,
-          description: data.weather[0].main
-        };
-        setWeatherData(weatherData2);
+      .then((res) => {
+        if (res.status === 200) {
+          return res.json().then(data => {
+            console.log(data);
+            const weatherData2 = {
+              city: data.name,
+              temp: data.main.temp,
+              icon: data.weather[0].icon,
+              description: data.weather[0].main,
+            };
+            setWeatherData(weatherData2);
+            setPending(false);
+          });
+        } else {
+          setError(true);
+        }
       });
   }, []);
+
+  console.log('pending', pending)
+  console.log("weatherData:", weatherData);
   return (
     <section>
       <PickCity action={handleCityChange} />
-      <WeatherSummary city={weatherData.city} temp={weatherData.temp} icon={weatherData.icon} description={weatherData.description} />
-      <Loader />
+      {weatherData && !pending && <WeatherSummary {...weatherData}
+      />}
+      {!error && pending && <Loader />}
+      {error && <ErrorBox />}
     </section>
   )
 };
